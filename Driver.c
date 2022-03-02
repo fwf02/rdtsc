1b4cae3a34f8fe317b0a52f710132585d518d60a
dani <wfqwqfwqf@gmail.com>
2022-03-02 02:10:37 +0200

    Update Driver.c

--- Driver.c
+++ Driver.c
@@ -10,7 +10,7 @@ UNICODE_STRING rDevices, rDosDevices;
 
 VOID Timer(PDRIVER_OBJECT pDriverObject)
 {
-	KeSetSystemAffinityThread(0); // 0 for HyperTherading On / 1 for HyperTherading off
+	KeSetSystemAffinityThread(0); // 0 for hyperthreading On / 1 for hyperthreading off
 	PLARGE_INTEGER PerformanceFrequency; 
 	KeQueryPerformanceCounter(&PerformanceFrequency); 
 	LARGE_INTEGER Result;
