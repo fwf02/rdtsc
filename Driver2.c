92648cc6a17effae9f1c232bef67a73d84d94f8a
dani <wfqwqfwqf@gmail.com>
2022-03-02 02:12:48 +0200

    reading RDTSC through KeStallExecutionProcessor

--- Driver2.c
+++ Driver2.c
@@ -10,7 +10,7 @@ UNICODE_STRING rDevices, rDosDevices;
 
 VOID Timer(PDRIVER_OBJECT pDriverObject)
 {
-	KeSetSystemAffinityThread(0); // 0 for hyperthreading On / 1 for hyperthreading off
+	KeSetSystemAffinityThread(0); // 0 for Hyperthreading On / 1 for Hyperthreading off
 	KeStallExecutionProcessor(1);
 	ULONG64 StartTime, EndTime;
 	StartTime = __rdtsc();
