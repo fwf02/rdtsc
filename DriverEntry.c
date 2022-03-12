0075e8bb0aa4f705acda45b1424f045f9f7fdc17
fwf02 <wfqwqfwqf@gmail.com>
2022-03-12 08:52:59 +0200

    faster then RDTSC

---
+++ DriverEntry.c
@@ -0,0 +1,46 @@
+#include <ntddk.h>
+
+PDEVICE_OBJECT pDeviceObject;
+UNICODE_STRING rDevices, rDosDevices;
+
+
+
+
+
+
+VOID Timer(PDRIVER_OBJECT pDriverObject)
+{
+	KeSetSystemAffinityThread(0); // 0 for HyperThreading / 1 for off HyperThreading
+	KeStallExecutionProcessor(1);
+	ULONG64 StartTime, EndTime;
+	StartTime = __readcr0();
+	EndTime = StartTime + KeGetPcr()->StallScaleFactor * 1;
+	return EndTime;
+}
+
+
+
+
+NTSTATUS UnloadDriver(PDRIVER_OBJECT pDriverObject)
+{
+	DbgPrintEx(0, 0, "Unloading Timer!\n");
+	IoDeleteSymbolicLink(&rDosDevices);
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath) 
+{
+	Timer(pDriverObject);
+
+	DbgPrintEx(0, 0, "Timer Loaded!\n");
+
+
+	RtlInitUnicodeString(&rDevices, L"\\Device\\Timer");
+	RtlInitUnicodeString(&rDosDevices, L"\\DosDevices\\Timer");
+
+	IoCreateSymbolicLink(&rDosDevices, &rDevices);
+
+	
+	pDriverObject->DriverUnload = UnloadDriver;
+
+	return STATUS_SUCCESS;
+}
