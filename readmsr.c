c2392ef7a7fd61ee0e794ddba38aa83e9e6f470b
fwf02 <wfqwqfwqf@gmail.com>
2022-03-19 13:53:54 +0200

    Read the CPU performance counters only for amd

--- readmsr.c
+++ readmsr.c
@@ -10,7 +10,6 @@ UNICODE_STRING rDevices, rDosDevices;
 
 VOID readmsr(PDRIVER_OBJECT pDriverObject)
 {
-	KeSetSystemAffinityThread(0); // 0 for HyperThreading / 1 for off HyperThreading
 	KeStallExecutionProcessor(1);
 	ULONG64 StartTime, EndTime;
 	StartTime = __readmsr(0x12); // test __readmsr(0x13); as well
