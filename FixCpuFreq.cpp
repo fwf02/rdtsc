925c74a48286ee46050c7bff07d358a81d497f43
fwf02 <wfqwqfwqf@gmail.com>
2023-03-28 10:11:29 +0000

    FixCpuFreq.cpp
    fix cpu freq in all processors

--- FixCpuFreq.cpp
+++ FixCpuFreq.cpp
@@ -4,111 +4,6 @@
 
 
 
-typedef struct ServiceDescriptorEntry {
-    unsigned int* ServiceTableBase;
-    unsigned int* ServiceCounterTableBase; //Used only in checked build   
-    unsigned int NumberOfServices;
-    unsigned char* ParamTableBase;
-} ServiceDescriptorTableEntry_t, * PServiceDescriptorTableEntry_t;
-
-//__declspec(dllimport)  ServiceDescriptorTableEntry_t KeServiceDescriptorTable;
-//#define SYSTEMSERVICE(_function)  KeServiceDescriptorTable.ServiceTableBase[ *(PULONG)((PUCHAR)_function+1)]  
-
-typedef enum class _SYSTEM_INFORMATION_CLASS {
-
-    SystemBasicInformation,
-    SystemProcessorInformation,
-    SystemPerformanceInformation,
-    SystemTimeOfDayInformation,
-    SystemPathInformation,
-    SystemProcessInformation,
-    SystemCallCountInformation,
-    SystemDeviceInformation,
-    SystemProcessorPerformanceInformation,
-    SystemFlagsInformation,
-    SystemCallTimeInformation,
-    SystemModuleInformation,
-    SystemLocksInformation,
-    SystemStackTraceInformation,
-    SystemPagedPoolInformation,
-    SystemNonPagedPoolInformation,
-    SystemHandleInformation,
-    SystemObjectInformation,
-    SystemPageFileInformation,
-    SystemVdmInstemulInformation,
-    SystemVdmBopInformation,
-    SystemFileCacheInformation,
-    SystemPoolTagInformation,
-    SystemInterruptInformation,
-    SystemDpcBehaviorInformation,
-    SystemFullMemoryInformation,
-    SystemLoadGdiDriverInformation,
-    SystemUnloadGdiDriverInformation,
-    SystemTimeAdjustmentInformation,
-    SystemSummaryMemoryInformation,
-    SystemNextEventIdInformation,
-    SystemEventIdsInformation,
-    SystemCrashDumpInformation,
-    SystemExceptionInformation,
-    SystemCrashDumpStateInformation,
-    SystemKernelDebuggerInformation,
-    SystemContextSwitchInformation,
-    SystemRegistryQuotaInformation,
-    SystemExtendServiceTableInformation,
-    SystemPrioritySeperation,
-    SystemPlugPlayBusInformation,
-    SystemDockInformation,
-    SystemPowerInformation,
-    SystemProcessorSpeedInformation,
-    SystemCurrentTimeZoneInformation,
-    SystemLookasideInformation
-
-
-} SYSTEM_INFORMATION_CLASS, * PSYSTEM_INFORMATION_CLASS;
-
-
-
-
-typedef struct _SYSTEM_MODULE_ENTRY
-{
-    HANDLE Section;
-    PVOID MappedBase;
-    PVOID ImageBase;
-    ULONG ImageSize;
-    ULONG Flags;
-    USHORT LoadOrderIndex;
-    USHORT InitOrderIndex;
-    USHORT LoadCount;
-    USHORT OffsetToFileName;
-    UCHAR FullPathName[256];
-} SYSTEM_MODULE_ENTRY, * PSYSTEM_MODULE_ENTRY;
-
-
-typedef struct _SYSTEM_MODULE_INFORMATION
-{
-    ULONG Count;
-    SYSTEM_MODULE_ENTRY Module[1];
-} SYSTEM_MODULE_INFORMATION, * PSYSTEM_MODULE_INFORMATION;
-
-NTSYSAPI
-NTSTATUS
-NTAPI ZwQuerySystemInformation(
-    IN SYSTEM_INFORMATION_CLASS SystemInformationClass,
-    IN PVOID SystemInformation,
-    IN ULONG SystemInformationLength,
-    OUT PULONG ReturnLength);
-
-
-typedef NTSTATUS(*ZWQUERYSYSTEMINFORMATION) (
-    SYSTEM_INFORMATION_CLASS SystemInformationCLass,
-    PVOID SystemInformation,
-    ULONG SystemInformationLength,
-    PULONG ReturnLength);
-
-extern ZWQUERYSYSTEMINFORMATION UD_ZwQuerySystemInformation;
-
-
-ZWQUERYSYSTEMINFORMATION UD_ZwQuerySystemInformation = 0;
 
 typedef struct _PROCESSOR_POWER_INFORMATION {
 	ULONG Number;
@@ -121,7 +16,6 @@ typedef struct _PROCESSOR_POWER_INFORMATION {
 
 
 // thanks to nbourdau https://github.com/mmlabs-mindmaze/mmlib/blob/0ae40719580cfaca353dde4d43b1779330404957/src/clock-win32.c
-// exovalqo https://github.com/exovalqo/EAC-Hook-Driver/blob/c01deddcb18bb7dd903eeaf43d3246afd123eb8e/EAC-Hook-Driver/main.cpp
 
 #define PICOSEC_IN_SEC  1000000000000LL
 #define MHZ_IN_HZ       1000000
@@ -132,10 +26,6 @@ static int64_t cpu_cycle_tick_psec; // Time of a cpu cycle in picosecond
 static
 void determine_cpu_cycle_rate(void)
 {
-    UNICODE_STRING function;
-    RtlInitUnicodeString(&function, L"ZwQuerySystemInformation");
-
-    UD_ZwQuerySystemInformation = (ZWQUERYSYSTEMINFORMATION)MmGetSystemRoutineAddress(&function);
 
 	int num_proc;
 	PROCESSOR_POWER_INFORMATION* proc_pwr_info;
